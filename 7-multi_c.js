3e55a6165063df8840745aa94627d4e3d6f17bec
Eyob Abera <eyobaberabekru@gmail.com>
2025-05-25 18:15:48 +0300

    corrected to count 3 times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,7 @@
 const arg = process.argv[2];
 const count = parseInt(arg);
 
-if (isNaN(count) || count <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (!isNaN(count) && count > 0) {
   let i = 0;
   while (i < count) {
     console.log("C is fun");
